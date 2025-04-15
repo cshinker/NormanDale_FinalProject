ac61a3a8c0770457f7dc9a4023bb19f6a8cc9893
Cam Shinker <shinkecj@mail.uc.edu>
2025-04-15 08:24:13 -0400

    Add project files.

---
+++ NormanDale_FinalProject/function_package/shinker.py
@@ -0,0 +1,13 @@
+# File Name: shinker.py
+# Student Name: Cam Shinker, Luke elmore
+# email: shinkecj@mail.uc.edu, elmorels@mail.uc.edu
+# Assignment Number: Final Project
+# Due Date: 4/24/2025
+# Course #/Section: IS 4010-002
+# Semester/Year: Spring 2025
+# Brief Description of the assignment: Decrypt 2 files and take a photo according to the decrypted data.
+
+# Brief Description of what this module does: Contains Cam's work on the functions
+# Citations: 
+
+# Anything else that's relevant:
---
+++ NormanDale_FinalProject/main_package/main.py
@@ -0,0 +1,17 @@
+# File Name: main.py
+# Student Name: Cam Shinker, Luke elmore
+# email: shinkecj@mail.uc.edu, elmorels@mail.uc.edu
+# Assignment Number: Final Project
+# Due Date: 4/24/2025
+# Course #/Section: IS 4010-002
+# Semester/Year: Spring 2025
+# Brief Description of the assignment: Decrypt 2 files and take a photo according to the decrypted data.
+
+# Brief Description of what this module does: Contains the entry point code for the assignment
+# Citations: 
+
+# Anything else that's relevant:
+
+
+if __name__ == "__main__":
+    pass
